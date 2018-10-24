4a8f0f919c9797d8c24bebcdb7480a27e63579f8
Cassandra  Evans <cassie@Cassandras-MacBook-Pro.local>
2018-10-24 19:21:25 +0100

    First commit

---
+++ src/rapha-core-de-300x50/js/banner.animation.js
@@ -0,0 +1,108 @@
+'use strict';
+
+/**
+ * Run the animation functions.
+ */
+Banner.prototype.start = function() {
+    this.banner = document.querySelector('.banner');
+
+    this.bannerWidth = this.banner.offsetWidth;
+    this.bannerHeight = this.banner.offsetHeight;
+
+    // Image array for preloading
+    this.images = [
+        'images/img1.jpg'
+    ];
+
+    var _this = this;
+    this.preloadImages(this.images, function() {
+        _this.createElements();
+        _this.setup();
+        _this.hidePreloader();
+        _this.animate();
+        _this.bindEvents();
+    });
+};
+
+/**
+ * Create dom elements.
+ */
+Banner.prototype.createElements = function() {
+
+};
+
+/**
+ * Setup initial element states.
+ */
+Banner.prototype.setup = function() {
+
+};
+
+/**
+ * Hide the preloader.
+ */
+Banner.prototype.hidePreloader = function() {
+    TweenLite.to('.preloader', 1, { autoAlpha: 0 });
+};
+
+/**
+ * Animation timeline.
+ */
+Banner.prototype.animate = function() {
+
+
+
+    var _this = this;
+
+    function loop() {
+        _this.timeline.gotoAndPlay('start');
+    }
+    var loops = 0;
+    var maxLoops = 1;
+
+    var banner = document.querySelector('.banner')
+    var CTA = document.querySelector(".cta");
+
+    // // pauses ad and changes CTA hover effect on mouseover
+    // banner.onmouseover = function() {
+    //     this.timeline.pause();
+    //     CTA.classList.add("hoverCTA");
+    // }.bind(_this)
+    // banner.onmouseleave = function() {
+    //     if (loops < maxLoops) {
+    //         this.timeline.play();
+    //     }
+    //     CTA.classList.remove("hoverCTA");
+    // }.bind(_this)
+
+    // var imageTimeline = new TimelineMax({})
+    //     .add(TweenMax.from(img1, 1, { opacity: 0, ease: Expo.ease }, ))
+    //     .add(TweenMax.from(img1, 8, { scale: 1.1, ease: Expo.ease, delay: -1 }, ))
+
+    var textTimeline = new TimelineMax({})
+        .add(TweenMax.set(".banner", { autoAlpha: 1 }))
+        .add(TweenMax.from("#txt1", 1, { opacity: 0, ease: Expo.ease, delay: 0.5 }, ))
+        .add(TweenMax.to("#txt1", 1, { opacity: 0, ease: Expo.easeOut, delay: 2.5 }))
+        .add(TweenMax.from("#txt2", 1, { opacity: 0, ease: Expo.ease, delay: 0.5 }, ))
+        .add(TweenMax.to("#txt2", 1, { opacity: 0, ease: Expo.easeOut, delay: 2.5 }))
+        .add(TweenMax.from(logo, 1, { opacity: 0, ease: Expo.ease, delay: -.5 }, ))
+        .add(TweenMax.to("#exploreMask", 1.5, { attr: { x: 0 }, ease: Expo.ease }, ))
+        .add(TweenMax.from(cta, 1, { opacity: 0, ease: Expo.ease }, ))
+        .call(function() {
+
+            if (loops === maxLoops) {
+                this.timeline.pause();
+            }
+            loops++;
+
+        }.bind(_this))
+        .add(TweenMax.to([cta, "logocont", ], 1, { opacity: 0, ease: Expo.easeOut, delay: 1 }))
+
+
+    this.timeline = new TimelineMax({ repeat: maxLoops })
+        .timeScale(1)
+        .addLabel('start', 0)
+        .add(textTimeline, 0)
+
+
+};
---
+++ src/rapha-core-en-160x600/js/banner.animation.js
@@ -0,0 +1,129 @@
+'use strict';
+
+/**
+ * Run the animation functions.
+ */
+Banner.prototype.start = function() {
+    this.banner = document.querySelector('.banner');
+
+    this.bannerWidth = this.banner.offsetWidth;
+    this.bannerHeight = this.banner.offsetHeight;
+
+    // Image array for preloading
+    this.images = [
+        'images/cta.svg',
+        'images/explore.svg',
+        'images/logo.svg',
+        'images/txt1.svg',
+        'images/image.jpg',
+    ];
+
+    var _this = this;
+    this.preloadImages(this.images, function() {
+        _this.createElements();
+        _this.setup();
+        _this.hidePreloader();
+        _this.animate();
+        _this.bindEvents();
+    });
+};
+
+/**
+ * Create dom elements.
+ */
+Banner.prototype.createElements = function() {
+    this.video = this.smartObject({
+        parent: this.banner,
+        id: 'video',
+        type: 'video',
+        autoplay: false, // If not set video will not autoplay
+        loop: true, // Default is no loop
+        controls: false, //Â If you set to true, the browser will offer controls buttons for the video
+        muted: true, // If not set the audio will be played with the video
+        poster: 'images/image.jpg', // Shows an image instead of first frame while the video is downloading, or until the user hits the play button.
+        preload: 'auto', // Possible values: none, metadata, auto.Â The preload attribute is ignored if you use autoplay.
+        playsinline: true, // Set to true if you want to use muted autoplay on mobile. It keeps a video inline because by default videos on iOS will play full screen.
+        //src: 'videos/core.mp4', // Place your videos in the separate "videos" folder to follow Bannertime structure.
+        sources: [{
+                url: Enabler.getUrl('videos/core.mp4'),
+                type: 'video/mp4'
+            },
+            {
+                url: Enabler.getUrl('videos/core.webm'),
+                type: 'video/webm'
+            }
+        ], // It'sÂ always recommended to use multiple sources. For support across all major browsersÂ use MP4 and either WebM or Ogg.
+        width: "102%",
+        height: "102%",
+        top: "-1%",
+        left: "-1%"
+
+    });
+
+    var _this = this;
+    Enabler.loadModule(studio.module.ModuleId.VIDEO, function() {
+        studio.video.Reporter.attach('video_1', _this.video);
+    });
+
+};
+
+/**
+ * Setup initial element states.
+ */
+Banner.prototype.setup = function() {
+    var _this = this;
+
+    this.video.addEventListener('canplaythrough', function() {
+        _this.timeline.play();
+    });
+};
+
+/**
+ * Hide the preloader.
+ */
+Banner.prototype.hidePreloader = function() {
+
+};
+
+/**
+ * Animation timeline.
+ */
+Banner.prototype.animate = function() {
+
+    var _this = this;
+
+    function loop() {
+        _this.timeline.gotoAndPlay('start');
+    }
+    var loops = 0;
+    var maxLoops = 2;
+
+    var video = document.getElementById("video");
+
+
+
+    this.timeline = new TimelineMax({ paused: true, repeat: maxLoops })
+        .addLabel('start', 0)
+        .call(function() {
+            video.currentTime = 0;
+            video.play();
+        }.bind(_this))
+        .set([".banner"], { autoAlpha: 1 })
+        .from("#txt1", 0.6, { opacity: 0, ease: Expo.ease, delay: 0.2 }, )
+        .to("#txt1", 0.6, { opacity: 0, ease: Expo.easeOut, delay: 4.2 })
+        .from(logo, 0.7, { opacity: 0, ease: Expo.ease, delay: -.3 }, )
+        .to("#exploreMask", 1, { attr: { x: 0 }, ease: Expo.ease }, )
+        .from(cta, 0.6, { opacity: 0, scale: 0.9, ease: Expo.ease, delay: -.3 }, )
+        .call(function() {
+
+            if (loops === maxLoops) {
+                this.timeline.pause();
+
+                setTimeout(function() { video.pause(); }, 1000);
+
+            }
+            loops++;
+
+        }.bind(_this))
+        .to([cta, "#logocont"], 0.5, { opacity: 0, ease: Expo.easeOut, delay: 1.5 });
+};
